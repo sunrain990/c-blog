330062d1c26364f7c06c5c2dbcba4a2656d92ca9
sunrain990 <409747494@qq.com>
2016-09-07 16:47:23 +0800

    init c-blog

---
+++ src/components/Home/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <Sidebar :index-img="indexImg"></Sidebar>
+    data : {{ data | json }}
+</template>
+
+<script>
+    import Sidebar from './sidebar.vue'
+    import { getDemosData } from '../../vuex/getters'
+    import { getDemo } from '../../vuex/actions'
+
+    export default {
+        components: {
+            Sidebar
+        },
+        vuex: {
+            getters: {
+                data: getDemosData
+            },
+            actions: {
+                getDemo
+            }
+        },
+        created() {
+            this.getDemo()
+        },
+        methods: {
+
+        }
+    }
+</script>
